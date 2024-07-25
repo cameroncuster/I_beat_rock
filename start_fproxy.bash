54c3497d7e2276334b53fa02c2b1c4722f1deb9f
Cameron Custer <custer.cameron@gmail.com>
2024-07-24 20:11:01 -0700

    add requirements

--- start_fproxy.bash
+++ start_fproxy.bash
@@ -1,6 +1,4 @@
 #!/bin/bash
 curl -o fproxy.py https://raw.githubusercontent.com/cameroncuster/I_beat_rock/main/fproxy.py
-pip install httpx
-pip install aiohttp
-pip install asyncio
+pip install -r requirements.txt
 python3 fproxy.py

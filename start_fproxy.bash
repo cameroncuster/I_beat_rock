58770182ce8e672b17884a0e331dbab1d6302820
Cameron Custer <custer.cameron@gmail.com>
2024-08-01 10:33:56 -0700

    work from the home directory

--- start_fproxy.bash
+++ start_fproxy.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd ~
 curl -o fproxy.py https://raw.githubusercontent.com/cameroncuster/I_beat_rock/main/fproxy.py
 sudo apt install python3-httpx -y
 sudo apt install python3-aiohttp -y
--- start_orchestrator.bash
+++ start_orchestrator.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd ~
 curl -o run_orchestrator.py https://raw.githubusercontent.com/cameroncuster/I_beat_rock/main/run_orchestrator.py
 sudo apt install python3-httpx -y
 sudo apt install python3-aiohttp -y

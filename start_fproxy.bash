3f677fc9e1b248b37d2a7f0a6fba2fcf9e1bebed
Cameron Custer <custer.cameron@gmail.com>
2024-08-01 10:44:14 -0700

    update the home dir

--- start_fproxy.bash
+++ start_fproxy.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
+curl -o ~/fproxy.py https://raw.githubusercontent.com/cameroncuster/I_beat_rock/main/fproxy.py
 cd ~
-curl -o fproxy.py https://raw.githubusercontent.com/cameroncuster/I_beat_rock/main/fproxy.py
 sudo apt install python3-httpx -y
 sudo apt install python3-aiohttp -y
 python3 fproxy.py
--- start_orchestrator.bash
+++ start_orchestrator.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
+curl -o ~/run_orchestrator.py https://raw.githubusercontent.com/cameroncuster/I_beat_rock/main/run_orchestrator.py
 cd ~
-curl -o run_orchestrator.py https://raw.githubusercontent.com/cameroncuster/I_beat_rock/main/run_orchestrator.py
 sudo apt install python3-httpx -y
 sudo apt install python3-aiohttp -y
 python3 run_orchestrator.py

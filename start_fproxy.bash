67d32cd6733c7a022678a22757c56194f3e7044a
Cameron Custer <custer.cameron@gmail.com>
2024-08-03 21:33:29 -0700

    nits

--- start_fproxy.bash
+++ start_fproxy.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -o ~/fproxy.py https://raw.githubusercontent.com/cameroncuster/I_beat_rock/main/fproxy.py
+curl -o fproxy.py https://raw.githubusercontent.com/cameroncuster/I_beat_rock/main/fproxy.py
 sudo apt install python3-httpx -y
 sudo apt install python3-aiohttp -y
 python3 fproxy.py
--- start_orchestrator.bash
+++ start_orchestrator.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -o ~/run_orchestrator.py https://raw.githubusercontent.com/cameroncuster/I_beat_rock/main/run_orchestrator.py
+curl -o run_orchestrator.py https://raw.githubusercontent.com/cameroncuster/I_beat_rock/main/run_orchestrator.py
 sudo apt install python3-httpx -y
 sudo apt install python3-aiohttp -y
 python3 run_orchestrator.py
